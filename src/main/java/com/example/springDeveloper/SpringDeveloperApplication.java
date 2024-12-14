02509edc440c563c43fbd0bd714bd47f4f779a0b
Renan da S. Silvaira <silveira.s.renan@gmail.com>
2024-12-13 21:18:29 -0300

    primeito commit

---
+++ src/main/java/com/example/springDeveloper/SpringDeveloperApplication.java
@@ -0,0 +1,13 @@
+package com.example.springDeveloper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDeveloperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDeveloperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springDeveloper/SpringDeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springDeveloper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
